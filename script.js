f81e3868c91222c65277d849f17bfc542deed1f2
Ali Aizad <aliaizad@protonmail.com>
2022-01-25 13:32:33 +0800

    Added some game features

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ const gameBoard = (function () {
 
     const _createRow = function () {
         const columns = Array.from(document.querySelectorAll('.board-column'));
-        console.log(columns);
         columns.forEach((column) => {
             for (i = 3; i > 0; i--) {
                 const row = document.createElement('div');
@@ -24,14 +23,60 @@ const gameBoard = (function () {
         });
     }
 
-    const createBoard = function () {
+    const _createBoard = (function () {
         _createColumn();
         _createRow();
-    }
+    }) ();
     
     return {
-        createBoard,
+
     }
 })();
 
-gameBoard.createBoard();
+const gameFlow = (function () {
+    //a function to return the text content of the array aka grid boxes;
+    const getStatus = function () {
+        const gameStatus = [];
+        const gridTexts = Array.from(document.querySelectorAll('.grid'))
+        gridTexts.forEach ((element) => {
+            gameStatus.push(element.innerText);
+        })
+        console.log(gameStatus);
+        return gameStatus;
+    }
+    //create a function that changes the symbol on the board
+    currentSymbol = 'X'
+    alternateSymbol = 'O'
+    roundCounter = 0;
+    const _selectSymbol = (function () {
+        const symbols = document.querySelectorAll('.gamebuts');
+        symbols.forEach ((symbol) => {
+            symbol.addEventListener('click', setSymbol);
+        })
+
+        function setSymbol (e) {
+            currentSymbol = e.target.innerText;
+        }   
+    })();
+    // create a function that change the divs text content when it is clicked
+    const _gridclick = (function () {
+        const gameBoxes = Array.from(document.querySelectorAll('.grid'));
+        gameBoxes.forEach((box) => {
+            box.addEventListener('click', putSymbol);
+        })
+
+        function putSymbol (e) {
+            if (roundCounter < 10) {
+                roundCounter++;
+            }
+        }
+    }) ();
+    //creates a function that counts the round
+
+}) ();
+
+const player = function (symbol) {
+    return {
+        symbol,
+    }
+};
